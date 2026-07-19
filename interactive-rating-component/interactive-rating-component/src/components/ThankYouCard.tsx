9b0d0e981ae62406a6343f29dc2562df99418209
nimanek <nima.nekooyi01@gmail.com>
2026-07-19 19:26:12 +0200

    fix: fixed pics uri

--- interactive-rating-component/interactive-rating-component/src/components/ThankYouCard.tsx
+++ interactive-rating-component/interactive-rating-component/src/components/ThankYouCard.tsx
@@ -9,7 +9,7 @@ const ThankYouCard = ({selectedRating}:ThankYouCardProps) => {
         <>
             <div className="bg-gray-800 h-91 w-90 rounded-2xl p-6 flex flex-col gap-2">
                 <div className="flex items-center justify-center">
-                    <img src="public/illustration-thank-you.svg" alt="Star" />
+                    <img src="/illustration-thank-you.svg" alt="Star" />
                 </div>
                 <div className="bg-gray-500/30 px-4 rounded-full py-2 text-orange-500 text-center mt-4">
                     <p>You selected <span>{selectedRating}</span> out of <span>{ratingNumbers.length}</span></p>
