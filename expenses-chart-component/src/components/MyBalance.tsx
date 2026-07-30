92f7fe3d4972b8b08cc36a784594c0974fdd8abd
nimanek <nima.nekooyi01@gmail.com>
2026-07-30 16:50:04 +0200

    fixed some styles

--- expenses-chart-component/src/components/MyBalance.tsx
+++ expenses-chart-component/src/components/MyBalance.tsx
@@ -3,8 +3,8 @@ const MyBalance = () => {
         <>
             <div className="bg-[#ED765E] flex items-center justify-between rounded-xl p-4 w-full h-22 text-white">
                 <div className="flex flex-col">
-                    <span className="text-sm">My balance</span>
-                    <p className="font-bold text-xl">$932.29</p>
+                    <span className="text-sm text-gray-50">My balance</span>
+                    <p className="font-bold text-2xl">$921.48</p>
                 </div>
                 <img src="./logo.svg" alt="logo" />
             </div>
