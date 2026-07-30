758f4a80ade6fd68ab08108b9348d369a68990a8
nimanek <nima.nekooyi01@gmail.com>
2026-07-30 17:00:48 +0200

    created footer

--- expenses-chart-component/src/App.tsx
+++ expenses-chart-component/src/App.tsx
@@ -1,15 +1,19 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Last7Days from "./components/main/Last7Days";
 import MyBalance from "./components/MyBalance";
 
 function App() {
     return (
-        <div className="flex items-center justify-center">
-            <div className="flex flex-col gap-4 mt-10 items-center justify-center w-full md:w-[50%]">
-                <MyBalance />
-                <Last7Days />
+        <>
+            <div className="flex items-center justify-center h-svh">
+                <div className="flex flex-col gap-4 mt-10 items-center justify-center w-full md:w-[50%]">
+                    <MyBalance />
+                    <Last7Days />
+                    <Footer />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
---
+++ expenses-chart-component/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+        <footer className="mt-10">
+            Challenge by{" "}
+            <a href="https://www.frontendmentor.io?ref=challenge">
+                Frontend Mentor
+            </a>
+            . Coded by{" "}
+            <a
+                className="text-[#be5945] hover:text-[#e65133]"
+                href="https://www.frontendmentor.io/profile/nimanek"
+            >
+                Nima Nk
+            </a>
+            .
+        </footer>
+    );
+};
+
+export default Footer;
