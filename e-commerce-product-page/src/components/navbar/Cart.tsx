1708d6eea771aa1e43597032fd83abb65ebf2f1c
nimanek <nima.nekooyi01@gmail.com>
2026-08-03 15:12:32 +0200

    made cart item UI and funcionality

--- e-commerce-product-page/src/components/navbar/Cart.tsx
+++ e-commerce-product-page/src/components/navbar/Cart.tsx
@@ -3,12 +3,22 @@ import type { NavbarProps } from "./Navbar";
 const Cart = ({ isCartOpen, setIsCartOpen, count, setCount }: NavbarProps) => {
 
 
+    // func for making numbers look like price.00
+    const formatPrice = (price:number)=>{
+        return price.toFixed(2)
+    }
 
-    console.log(count)
+    // item price
+    const itemPrice = 125
+    
+    // total price
+    const finalPrice = count * itemPrice
+
     return (
         <div className="relative">
             <button onClick={()=>setIsCartOpen(!isCartOpen)} className="cursor-pointer hover:bg-gray-100 p-2 rounded-full">
                 <img src="./icon-cart.svg" alt="cart" />
+                <span className="absolute bg-orange-500 text-white rounded-4xl w-4 h-4 text-xs top-0">{count}</span>
             </button>
 
             {isCartOpen && (
@@ -19,7 +29,20 @@ const Cart = ({ isCartOpen, setIsCartOpen, count, setCount }: NavbarProps) => {
                         <p className="h-20 text-gray-500 flex items-end justify-center"> Your cart is empty.</p>
                     ) : (
                         <>
-                            <p>123</p>
+                            <div className="text-gray-500 flex items-center justify-between p-4">
+
+                                <img className="h-14 rounded-md" src="./image-product-1-thumbnail.jpg" />
+
+                                <div className="flex flex-col items-start">
+                                    <p>Fall Limited Edition Sneakers</p>
+                                    <span className="flex items-center justify-center gap-2">${formatPrice(itemPrice)} x {count} <span className="font-bold text-black">${formatPrice(finalPrice)}</span>
+                                    </span>
+                                </div>
+
+                                <button>
+                                    <img src="../../../public/icon-delete.svg" alt="" />
+                                </button>
+                            </div>
                         </>
                     )}
                 </div>
