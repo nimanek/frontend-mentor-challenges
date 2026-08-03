f1efcafd783d6052539cf1dbe11ba321543025dc
nimanek <nima.nekooyi01@gmail.com>
2026-08-03 14:11:59 +0200

    made func for add & dec

--- e-commerce-product-page/src/components/AddToCart.tsx
+++ e-commerce-product-page/src/components/AddToCart.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 const AddToCart = () => {
+    const [count, setCount] = useState(0)
+
+
+    const handleAdd = ()=>{
+        setCount(prev => prev + 1)
+    }
+    
+    const handleDec = ()=>{
+        setCount(prev => prev - 1)
+    }
+
     return (
         <div>
             <div className="flex items-center gap-4 mt-4">
                 <div className="flex items-center justify-around w-60 py-2 rounded-sm bg-gray-100">
-                    <button className="font-bold text-2xl text-orange-500 cursor-pointer">
+                    <button onClick={handleDec} className="font-bold text-2xl text-orange-500 cursor-pointer disabled:text-orange-200" disabled={count=== 0}>
                         -
                     </button>
-                    <span>0</span>
-                    <button className="font-bold text-2xl text-orange-500 cursor-pointer">
+                    <span>{count}</span>
+                    <button onClick={handleAdd} className="font-bold text-2xl text-orange-500 cursor-pointer">
                         +
                     </button>
                 </div>
